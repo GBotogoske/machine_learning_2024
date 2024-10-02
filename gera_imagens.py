271fcf2e44306a4e27d4ede84680165ddf7e8387
Gabriel <gabrielbotogoske@gmail.com>
2024-10-02 13:28:51 -0300

    gera_imagens_begins

---
+++ gera_imagens.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[16]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from microboone_utils import *
+import pandas as pd
+from h5py import File
+
+
+# In[17]:
+
+
+#abre o arquivo
+f = File('/home/gabriel/Downloads/bnb_WithWire_00.h5','r')
+
+
+# In[18]:
+
+
+#mostra todas as tabelas disponiveis no arquivo .h5 aberto
+
+labels=[]
+sub_labels=[]
+for lines in f:
+        labels.append(lines)
+        aux=[]
+        for key in f[lines]:
+            aux.append(key)
+        sub_labels.append(aux)
+
+
+# In[19]:
+
+
+#montar uma tbela dos eventos disponiveis na file
+size=len(f[labels[1]]["event_id"][:])
+
+names_event=["run","subrun","event"]
+names_event_values=[[] for _ in range(len(names_event))]
+for i in range(size):
+    for j in range(len(names_event)):
+        names_event_values[j].append(f[labels[1]]["event_id"][i][j])
+
+temp_dict = {}
+for i, name in enumerate(names_event):
+    temp_dict[name] = names_event_values[i]
+
+ 
+event_table_pd = pd.DataFrame(temp_dict)
+names_event_values.clear()    
+
+
+# In[20]:
+
+
+#montar a tabela de wire
+N_fotos=3
+for evento_index in range(N_fotos):
+    max=8256
+    init=max*evento_index
+    end=max*(evento_index+1) #tentar ver depois como pegar evento por evento melhor!!!! acabei de ver que sao 8256 linhas por evento
+
+    wire_table=f[labels[10]]
+    event_wire_plan=wire_table["event_id"][init:end]
+    local_plane = wire_table["local_plane"][init:end]
+    adc=wire_table["adc"][init:end]
+    adc = [adc.tolist() for adc in adc]
+
+    names_event=["run","subrun","event"]
+    names_event_values=[[] for _ in range(len(names_event))]
+    for i in range(max):
+        for j in range(len(names_event)):
+            names_event_values[j].append(wire_table["event_id"][i+init][j])
+
+    temp_dict = {}
+    for i, name in enumerate(names_event):
+        temp_dict[name] = names_event_values[i]
+
+    temp_dict["local_plane"]=local_plane.flatten()
+    temp_dict["adc"]=adc
+
+
+    wire_table_pd = pd.DataFrame(temp_dict)
+
+    adc.clear()
+    event_wire_plan=None
+    local_plane=None
+    wire_table=None
+
+
+    # In[21]:
+
+
+    #vamos filtar por eventos, depois esse bloco tem que ser um for, junto com a parte de cima ... uhmmm
+    my_index=evento_index
+    this_run=event_table_pd["run"][my_index]
+    this_subrun=event_table_pd["subrun"][my_index]
+    this_event=event_table_pd["event"][my_index]
+
+    wire_table_pd=wire_table_pd.query("run==@this_run and subrun==@this_subrun and event==@this_event")
+
+
+    # In[22]:
+
+
+    planeadcs=[]
+    for p in range(0,nplanes()):
+        table_aux=wire_table_pd.query("local_plane==@p")
+        aux=[]
+        for j in range(len(table_aux)):
+            aux.append(table_aux.iloc(0)[j]["adc"])
+        planeadcs.append(np.array(aux))
+
+    wire_table_pd=wire_table_pd.drop(columns="adc")
+
+
+    # In[23]:
+
+
+    from skimage.measure import block_reduce
+    f_downsample = 6
+    for p in range(0,nplanes()):
+        planeadcs[p] = block_reduce(planeadcs[p], block_size=(1,f_downsample), func=np.sum)
+
+    adccutoff = 10.*f_downsample/6.
+    adcsaturation = 100.*f_downsample/6.
+    for p in range(0,nplanes()):
+        planeadcs[p][planeadcs[p]<adccutoff] = 0
+        planeadcs[p][planeadcs[p]>adcsaturation] = adcsaturation
+
+
+    # In[29]:
+
+
+    evt_id = [this_run, this_subrun, this_event]
+    zmax = adcsaturation
+
+    print("Run / Sub / Event : %i / %i / %i - saturation set to ADC sum=%.2f" % (evt_id[0], evt_id[1], evt_id[2], zmax))
+
+    # Plota a primeira imagem
+    plt.imshow(planeadcs[0].T, vmin=0, vmax=zmax, origin='lower', cmap='jet')
+    # Salva a primeira imagem
+    plt.tight_layout()
+    plt.savefig(f"./figuras/event_{evt_id[0]}_{evt_id[1]}_{evt_id[2]}_plane_0.png", bbox_inches='tight')
+    plt.clf()  # Limpa a figura para a próxima imagem
+
+    # Plota a segunda imagem
+    plt.imshow(planeadcs[1].T, vmin=0, vmax=zmax, origin='lower', cmap='jet')
+    # Salva a segunda imagem
+    plt.tight_layout()
+    plt.savefig(f"./figuras/event_{evt_id[0]}_{evt_id[1]}_{evt_id[2]}_plane_1.png", bbox_inches='tight')
+    plt.clf()  # Limpa a figura para a próxima imagem
+
+    # Plota a terceira imagem
+    plt.imshow(planeadcs[2].T, vmin=0, vmax=zmax, origin='lower', cmap='jet')
+    # Salva a terceira imagem
+    plt.tight_layout()
+    plt.savefig(f"./figuras/event_{evt_id[0]}_{evt_id[1]}_{evt_id[2]}_plane_2.png", bbox_inches='tight')
+    plt.clf()  # Limpa a figura para a próxima imagem
+
